98a9609980380035198da2afd3db7641de397d50
XoDeR <xoder.vs@gmail.com>
2025-08-29 15:52:59 +0200

    added express with cors and dotenv

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,17 @@
-console.log("server is running");
+import express from "express"
+import type { Application } from "express";
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app: Application = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors({ exposedHeaders: ["Content-Disposition"] }))
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`)
+});
+
