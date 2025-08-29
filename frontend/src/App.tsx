e581a031819c0139816ee7813f37aaab8ecc48ec
XoDeR <xoder.vs@gmail.com>
2025-08-29 17:55:31 +0200

    added react-router-dom, empty pages and frontend navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import { Toaster } from "@/components/ui/sonner";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import './App.css'
+import CodeEditor from "./pages/CodeEditor";
+import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 
-function App() {
-
+const App = () => {
   return (
     <>
-      App
+      <Toaster />
+      <BrowserRouter>
+        <div className="min-h-screen bg-background">
+          <Routes>
+            <Route path="/" element={<CodeEditor />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/dashboard" element={
+              <Dashboard />
+            } />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
