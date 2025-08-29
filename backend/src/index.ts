9b12172f9071a3de48e27487b6088afc39de51f3
XoDeR <xoder.vs@gmail.com>
2025-08-29 16:30:44 +0200

    installed clerk for auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import type { Application } from "express";
+import { clerkMiddleware } from "@clerk/express";
 import cors from "cors"
 import dotenv from "dotenv"
 
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({ exposedHeaders: ["Content-Disposition"] }))
 app.use(express.json());
+app.use(clerkMiddleware());
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`)
